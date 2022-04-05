5468da6d0eaeda19e3008855d587bfed9734f58f
MaganaAsiati <assia.magana@gmail.com>
2022-04-05 23:16:50 +0200

    modify linter file, style close icon and make overlay screen disapear on link click

--- js/script.js
+++ js/script.js
@@ -1,11 +1,24 @@
 const menuButtonOpen = document.getElementById('mobile-menu');
 const menuButtonClose = document.getElementById('mobile-menu-exit');
 const navListMenu = document.getElementById('nav-list-menu');
+const navItems = document.querySelectorAll('.nav-item');
 
-menuButtonOpen.addEventListener('click', () => {
-  navListMenu.classList.add('open');
-});
+function menuToggle() {
+  if (navListMenu.classList.contains('open')) {
+    navListMenu.classList.remove('open');
+    menuButtonOpen.style.display = 'block';
+    menuButtonClose.style.display = 'none';
+    navListMenu.style.display = 'none';
+  } else {
+    navListMenu.classList.add('open');
+    menuButtonOpen.style.display = 'none';
+    menuButtonClose.style.display = 'block';
+    navListMenu.style.display = 'block';
+  }
+}
 
-menuButtonClose.addEventListener('click', () => {
-  navListMenu.classList.remove('open');
+menuButtonOpen.addEventListener('click', menuToggle);
+menuButtonClose.addEventListener('click', menuToggle);
+navItems.forEach((navItem) => {
+  navItem.addEventListener('click', menuToggle);
 });
