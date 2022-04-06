51b4241a2a710c60729716049da04ac8e84c9c1c
MaganaAsiati <assia.magana@gmail.com>
2022-04-06 18:47:29 +0200

    add index.js file

---
+++ js/data.js
@@ -0,0 +1,72 @@
+/* eslint-disable no-tabs */
+const data = [
+  {
+    id: 1,
+    image: 'img/Snapshoot-Portfolio1.png',
+    name: 'Tonic',
+    description: {
+      type: 'CANOPY',
+      interface: 'Backend Dev',
+      year: 2015,
+    },
+    fullDesciption:
+			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae eius quae optio et quas, quibusdam quis excepturi omnis? Quos, officia accusamus. Aperiam, molestiae beatae numquam vitae modi est quod laboriosam! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi voluptatum quibusdam adipisci voluptates magnam ipsam dolores ad! Adipisci veritatis, sint perspiciatis reiciendis omnis, maiores incidunt, ex aut inventore deleniti nemo. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil mollitia deleniti nobis facilis delectus consequuntur, culpa commodi quia repellendus molestias doloribus praesentium voluptas nisi sint, similique repellat atque maiores aperiam.    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita ex quam officia ea dolor. Ea quasi in id dolorum ',
+    detail:
+			'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    technologies: ['Html', 'Css', 'Javascript', 'github', 'Bootstrap'],
+    liveDemo: 'https://maganaasiati.github.io/Portofolio/',
+    sourceCode: 'https://github.com/MaganaAsiati/Portofolio',
+  },
+  {
+    id: 2,
+    image: 'img/Snapshoot-Portfolio2.png',
+    name: 'Multi-Post Stories',
+    description: {
+      type: 'CANOPY',
+      interface: 'Backend Dev',
+      year: 2015,
+    },
+    fullDesciption:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    detail:
+      'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
+    technologies: ['Html', 'Css', 'Javascript', 'github', 'Bootstrap'],
+    liveDemo: 'https://maganaasiati.github.io/Portofolio/',
+    sourceCode: 'https://github.com/MaganaAsiati/Portofolio',
+  },
+  {
+    id: 3,
+    image: 'img/Snapshoot-Portfolio3.png',
+    name: 'Tonic',
+    description: {
+      type: 'CANOPY',
+      interface: 'Backend Dev',
+      year: 2015,
+    },
+    fullDesciption:
+			'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    detail:
+			'Exploring the future of media in Facebooks first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
+    technologies: ['Html', 'Css', 'Javascript', 'github', 'Bootstrap'],
+    liveDemo: 'https://maganaasiati.github.io/Portofolio/',
+    sourceCode: 'https://github.com/MaganaAsiati/Portofolio',
+  },
+  {
+    id: 4,
+    image: 'img/Snapshoot-Portfolio(5).png',
+    name: 'Multi-Post Stories',
+    description: {
+      type: 'CANOPY',
+      interface: 'Backend Dev',
+      year: 2015,
+    },
+    fullDesciption:
+			'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    detail:
+			'Exploring the future of media in Facebooks first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
+    technologies: ['Html', 'Css', 'Javascript', 'github', 'Bootstrap'],
+    liveDemo: 'https://maganaasiati.github.io/Portofolio/',
+    sourceCode: 'https://github.com/MaganaAsiati/Portofolio',
+  },
+];
+export default data;
