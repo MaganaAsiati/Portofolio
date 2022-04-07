ad070c88284dd5e7eacc6425cd104fe77fd4a776
MaganaAsiati <assia.magana@gmail.com>
2022-04-07 15:10:27 +0200

    add popup.css file and popup window

--- js/data.js
+++ js/data.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-tabs */
 const data = [
   {
     id: 1,
@@ -10,9 +9,9 @@ const data = [
       year: 2015,
     },
     fullDesciption:
-			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae eius quae optio et quas, quibusdam quis excepturi omnis? Quos, officia accusamus. Aperiam, molestiae beatae numquam vitae modi est quod laboriosam! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi voluptatum quibusdam adipisci voluptates magnam ipsam dolores ad! Adipisci veritatis, sint perspiciatis reiciendis omnis, maiores incidunt, ex aut inventore deleniti nemo. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil mollitia deleniti nobis facilis delectus consequuntur, culpa commodi quia repellendus molestias doloribus praesentium voluptas nisi sint, similique repellat atque maiores aperiam.    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita ex quam officia ea dolor. Ea quasi in id dolorum ',
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae eius quae optio et quas, quibusdam quis excepturi omnis? Quos, officia accusamus. Aperiam, molestiae beatae numquam vitae modi est quod laboriosam! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi voluptatum quibusdam adipisci voluptates magnam ipsam dolores ad! Adipisci veritatis, sint perspiciatis reiciendis omnis, maiores incidunt, ex aut inventore deleniti nemo. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil mollitia deleniti nobis facilis delectus consequuntur, culpa commodi quia repellendus molestias doloribus praesentium voluptas nisi sint, similique repellat atque maiores aperiam.    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita ex quam officia ea dolor. Ea quasi in id dolorum ',
     detail:
-			'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
     technologies: ['Html', 'Css', 'Javascript', 'github', 'Bootstrap'],
     liveDemo: 'https://maganaasiati.github.io/Portofolio/',
     sourceCode: 'https://github.com/MaganaAsiati/Portofolio',
@@ -44,9 +43,9 @@ const data = [
       year: 2015,
     },
     fullDesciption:
-			'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
     detail:
-			'Exploring the future of media in Facebooks first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
+      'Exploring the future of media in Facebooks first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
     technologies: ['Html', 'Css', 'Javascript', 'github', 'Bootstrap'],
     liveDemo: 'https://maganaasiati.github.io/Portofolio/',
     sourceCode: 'https://github.com/MaganaAsiati/Portofolio',
@@ -61,9 +60,9 @@ const data = [
       year: 2015,
     },
     fullDesciption:
-			'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
     detail:
-			'Exploring the future of media in Facebooks first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
+      'Exploring the future of media in Facebooks first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
     technologies: ['Html', 'Css', 'Javascript', 'github', 'Bootstrap'],
     liveDemo: 'https://maganaasiati.github.io/Portofolio/',
     sourceCode: 'https://github.com/MaganaAsiati/Portofolio',
